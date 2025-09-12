450a484af45f4288de17fdbf8b27b419587b3704
Sang Le <3lesang@gmail.com>
2025-09-12 21:21:16 +0700

    build: change to bun

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0fvujd0cvtg"
+    "revision": "0.5jb977ah3eg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
