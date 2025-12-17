ef7a3dcbed24f653c93a810a9775206b87b1c4d2
Sang Le <3lesang@email.com>
2025-12-17 11:52:12 +0700

    update type

--- src/queries/review.ts
+++ src/queries/review.ts
@@ -1,44 +1,44 @@
-import { queryOptions } from "@tanstack/react-query";
-import { PRODUCT_REVIEW_QUERY_KEY } from "@/constants";
 import axiosClient from "@/axios";
+import { PRODUCT_REVIEW_QUERY_KEY } from "@/constants";
+import { queryOptions } from "@tanstack/react-query";
 
 type PaginationResponse<T> = {
-  data: T[];
-  page: number;
-  page_size: number;
-  total_items: number;
-  total_pages: number;
+	data: T[];
+	page: number;
+	page_size: number;
+	total_items: number;
+	total_pages: number;
 };
 
 type ReviewData = {
-  id: number;
-  rating: number;
-  comment: string;
-  customer: {
-    id: number;
-    name: string;
-  };
-  files: string[];
+	id: number;
+	rating: number;
+	comment: string;
+	customer: {
+		id: number;
+		name: string;
+	};
+	files: string[];
 };
 
 export function getReviewsByProductQueryOptions({
-  page,
-  limit,
-  productId,
+	page,
+	limit,
+	productId,
 }: {
-  page: number;
-  limit: number;
-  productId: number;
+	page: number;
+	limit: number;
+	productId: number;
 }) {
-  return queryOptions({
-    queryKey: [PRODUCT_REVIEW_QUERY_KEY, productId, page, limit],
-    queryFn: () => {
-      return axiosClient.get<PaginationResponse<ReviewData>>(
-        `/reviews/products/${productId}`,
-        {
-          params: { page, page_size: limit },
-        },
-      );
-    },
-  });
+	return queryOptions({
+		queryKey: [PRODUCT_REVIEW_QUERY_KEY, productId, page, limit],
+		queryFn: () => {
+			return axiosClient.get<PaginationResponse<ReviewData>>(
+				`/reviews/products/${productId}`,
+				{
+					params: { page, page_size: limit },
+				},
+			);
+		},
+	});
 }
--- src/routes/(app)/products/$id/reviews/index.tsx
+++ src/routes/(app)/products/$id/reviews/index.tsx
@@ -4,22 +4,22 @@ import { createFileRoute } from "@tanstack/react-router";
 import z from "zod";
 
 const schema = z.object({
-  page: z.number().default(1),
-  limit: z.number().default(10),
-  query: z.string().default(""),
+	page: z.number().default(1),
+	limit: z.number().default(10),
+	query: z.string().default(""),
 });
 
 export const Route = createFileRoute("/(app)/products/$id/reviews/")({
-  component: ReviewListPage,
-  validateSearch: schema,
-  loaderDeps: ({ search }) => search,
-  loader: ({ context, deps, params }) => {
-    return context.queryClient?.ensureQueryData(
-      getReviewsByProductQueryOptions({
-        page: deps.page,
-        limit: deps.limit,
-        productId: params.id,
-      }),
-    );
-  },
+	component: ReviewListPage,
+	validateSearch: schema,
+	loaderDeps: ({ search }) => search,
+	loader: ({ context, deps, params }) => {
+		return context.queryClient?.ensureQueryData(
+			getReviewsByProductQueryOptions({
+				page: deps.page,
+				limit: deps.limit,
+				productId: Number(params.id),
+			}),
+		);
+	},
 });
