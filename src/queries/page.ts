e3ce3faac40c03ec52e9c1a9265395639afb9571
Sang Le <3lesang@email.com>
2025-12-20 08:25:02 +0700

    update page

--- src/queries/page.ts
+++ src/queries/page.ts
@@ -4,50 +4,52 @@ import axiosClient from "@/axios";
 import { STORE_PAGE_QUERY_KEY } from "@/constants";
 
 type PageData = {
-  id: number;
-  name: string;
-  slug: string;
+	id: number;
+	name: string;
+	slug: string;
 };
 
 type PaginationResponse<T> = {
-  data: T[];
-  page: number;
-  page_size: number;
-  total_items: number;
-  total_pages: number;
+	data: T[];
+	page: number;
+	page_size: number;
+	total_items: number;
+	total_pages: number;
 };
 
 type Params = {
-  page: number;
-  limit: number;
-  query: string;
+	page: number;
+	limit: number;
+	query: string;
 };
 
 export const getPagesQueryOptions = ({ page, limit, query }: Params) => {
-  return queryOptions({
-    queryKey: [STORE_PAGE_QUERY_KEY, page, limit, query],
-    queryFn: () => {
-      return axiosClient.get<PaginationResponse<PageData>>("/pages");
-    },
-  });
+	return queryOptions({
+		queryKey: [STORE_PAGE_QUERY_KEY, page, limit, query],
+		queryFn: () => {
+			return axiosClient.get<PaginationResponse<PageData>>("/pages", {
+				params: { page, page_size: limit },
+			});
+		},
+	});
 };
 
 export function getPageQueryOptions(pageID: string) {
-  return queryOptions({
-    queryKey: [STORE_PAGE_QUERY_KEY, pageID],
-    queryFn: () => {
-      return axiosClient.get<PageData>(`/pages/${pageID}`);
-    },
-  });
+	return queryOptions({
+		queryKey: [STORE_PAGE_QUERY_KEY, pageID],
+		queryFn: () => {
+			return axiosClient.get<PageData>(`/pages/${pageID}`);
+		},
+	});
 }
 
 export function getPageContentQueryOptions(pageId: string) {
-  return queryOptions({
-    queryKey: ["page-content", pageId],
-    queryFn: () => {
-      return axiosClient.get<JSONContent>(
-        `https://bucket.senhome.vn/page/${pageId}`,
-      );
-    },
-  });
+	return queryOptions({
+		queryKey: ["page-content", pageId],
+		queryFn: () => {
+			return axiosClient.get<JSONContent>(
+				`https://bucket.senhome.vn/page/${pageId}`,
+			);
+		},
+	});
 }
