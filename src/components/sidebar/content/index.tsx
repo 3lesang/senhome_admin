c33996de95dc2febc9cdd17759d8cb4713b965fe
Sang Le <3lesang@email.com>
2025-12-25 10:38:13 +0700

    update

--- src/components/sidebar/content/index.tsx
+++ src/components/sidebar/content/index.tsx
@@ -1,12 +1,12 @@
 import { SidebarContent } from "@/components/ui/sidebar";
 import ManageMenuGroup from "./manage";
-import OverviewMenuGroup from "./overview";
+// import OverviewMenuGroup from "./overview";
 import StoreMenuGroup from "./store";
 
 export default function ContentSidebar() {
 	return (
 		<SidebarContent>
-			<OverviewMenuGroup />
+			{/*<OverviewMenuGroup />*/}
 			<ManageMenuGroup />
 			<StoreMenuGroup />
 		</SidebarContent>
